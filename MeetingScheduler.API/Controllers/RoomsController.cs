316ac95b434f1da7927ea37ccb65b1e8df6156c4
yoranns <yoranns@gmail.com>
2025-07-29 16:25:58 -0300

    Adição de parametro de geração de documentação no projeto principal, adição de controlador da entidade Room e adição de configuração no json para acesso ao banco.

---
+++ MeetingScheduler.API/Controllers/RoomsController.cs
@@ -0,0 +1,37 @@
+﻿using MeetingScheduler.Data;
+using MeetingScheduler.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingScheduler.API.Controllers
+{
+    /// <summary>
+    /// Controlador da entidade Room
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private readonly MeetingSchedulerContext _context;
+        /// <summary>
+        /// Construtor do controlador da entidade Room
+        /// </summary>
+        /// <param name="context">Contexto de agendamento de reuinão que permite I/0 no BD</param>
+        public RoomsController(MeetingSchedulerContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Obtém todas as salas existentes
+        /// </summary>
+        /// <returns>Uma lista de todas as salas</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Room>>> GetAllRooms()
+        {
+            var rooms = await _context.Rooms.ToListAsync();
+            return Ok(rooms);
+        }
+
+    }
+}
