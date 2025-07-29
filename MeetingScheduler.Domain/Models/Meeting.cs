99bffd7e1b392a4701eea1cc699a288eb57baeb8
yoranns <yoranns@gmail.com>
2025-07-29 12:05:45 -0300

    - Remoção das classes autogeradas da API. - Remoção de instruções using desnecessárias nas classes de modelo. - (Commit anterior/inicial) adição das classes de entidade Room e Meeting e configuração da suas respectivas modelagens no contexto MeetingSchedulerContext.

--- MeetingScheduler.Domain/Models/Meeting.cs
+++ MeetingScheduler.Domain/Models/Meeting.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MeetingScheduler.Domain.Models
+﻿namespace MeetingScheduler.Domain.Models
 {
     public class Meeting
     {
